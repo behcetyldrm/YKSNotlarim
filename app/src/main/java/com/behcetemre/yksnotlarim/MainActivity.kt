cbb4eba3ba1d4fbf739db8167276e75ed0ee72b7
Behçet Emre Yıldırım <behcetyldrm10@gmail.com>
2026-02-18 23:06:44 +0300

    TopAppBar eklendi

--- app/src/main/java/com/behcetemre/yksnotlarim/MainActivity.kt
+++ app/src/main/java/com/behcetemre/yksnotlarim/MainActivity.kt
@@ -7,20 +7,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.behcetemre.yksnotlarim.ui.theme.YKSNotlarımTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             YKSNotlarımTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { TopAppBar() }
+                ) { innerPadding ->
                     Box(modifier = Modifier.padding(innerPadding)) {
 
                     }
@@ -28,4 +41,22 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar() {
+
+    CenterAlignedTopAppBar(
+        title = { Text(
+            text = "YKS Notlarım",
+            color = Color.White,
+            fontSize = 28.sp,
+            fontWeight = FontWeight.SemiBold
+        ) },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color(0xFFF44336)
+        ),
+        modifier = Modifier.clip(RoundedCornerShape(bottomStart = 24.dp, bottomEnd = 24.dp))
+    )
 }
